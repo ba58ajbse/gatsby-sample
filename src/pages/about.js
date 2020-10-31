cd5af5a65841a9877c079aea8767a92e0500c027
Administrator <admin@example.com>
2020-10-31 14:50:54 +0900

    create form

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const About = () => {
+  const [values, setValue] = useState({name: '', email: '', message: ''});
+
+  const handleChange = (e => {
+    setValue({...values, [e.target.name]: e.target.value});
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('お問い合わせありがとうございます')
+    setValue({name: '', email: '', message: ''});
+  }
+
   return (
     <div>
-      <h1>about page</h1>
+      <form
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        onSubmit={handleSubmit}
+      >
+        <div>
+          <input type="text" required name="name" placeholder="お名前" value={values.name} onChange={handleChange} />
+        </div>
+        <div>
+          <input type="email" name="email" placeholder="Email" value={values.email} onChange={handleChange} />
+        </div>
+        <div>
+          <textarea name="message" required placeholder="お問合せ内容" value={values.message} onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit">送信</button>
+        </div>
+      </form>
     </div>
   )
 }
