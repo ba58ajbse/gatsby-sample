2d4e959d252327d6c092a810c2e130ef12f69c33
Administrator <admin@example.com>
2020-11-03 12:51:35 +0900

    import overlay

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from 'gatsby'
+import Header from "./components/Header"
+import Overlay from './components/Overlay';
+import 'ress';
+import './css/style.css'
+import Meta from "./components/Meta";
 
 export default function Home() {
+  const [overlay, setOverlay] = useState(false);
+  const overlayToggle = () => setOverlay(!overlay);
   return (
     <div>
+      <Meta />
+      <Header toggle={overlayToggle} open={overlay} />
+      {overlay && <Overlay />}
       Hello world!
       <Link to='/about'>to about</Link>
     </div>
