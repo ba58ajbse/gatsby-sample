18d676ce094f57b0a6339a7c3269794b439938f5
Administrator <admin@example.com>
2020-11-03 12:50:44 +0900

    add overlay parts

---
+++ src/pages/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul className="nav-list">
+        <li className="nav-item">NEWS</li>
+        <li className="nav-item">ABOUT</li>
+        <li className="nav-item">COMPANY</li>
+        <li className="nav-item">CONTACT</li>
+      </ul>
+    </nav>
+  )
+}
+export default Nav;
---
+++ src/pages/components/Overlay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Nav from './Nav';
+
+const Overlay = () => {
+  return (
+    <div id="overlay">
+      <Nav />
+    </div>
+  )
+}
+export default Overlay;
