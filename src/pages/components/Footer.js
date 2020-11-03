efae6ed7494975289b37d71eed5c42b4f8e1b67e
Administrator <admin@example.com>
2020-11-03 22:26:37 +0900

    edit footer

---
+++ src/pages/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-info">
+        <p>〒123-4567 神奈川県横浜市○○○○○○○</p>
+        <p>TEL 03-0303-0303</p>
+      </div>
+    </footer>
+  )
+}
+export default Footer;
