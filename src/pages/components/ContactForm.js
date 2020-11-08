b33b2c70b758466734d5a33509e0a55f1a09ae20
Administrator <admin@example.com>
2020-11-08 14:04:11 +0900

    fix form by react hook form

--- src/pages/components/ContactForm.js
+++ src/pages/components/ContactForm.js
@@ -1,55 +1,74 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 import { navigate } from 'gatsby-link';
 
 
 const ContactForm = () => {
-  const [values, setValue] = useState({name: '', email: '', message: ''});
+  const { register, handleSubmit, reset, errors } = useForm();
 
-  const encode = (data) => {
-    return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
-    }
-    const handleChange = (e => {
-      setValue({...values, [e.target.name]: e.target.value});
-    });
+  // const encode = (data) => {
+  //   return Object.keys(data)
+  //     .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+  //     .join("&");
+  //   }
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      const form = e.target;
-      fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: encode({
-          "form-name": form.getAttribute("name"),
-          ...values
-        })
-      })
-        .then(() => navigate(form.getAttribute("action")))
-        .then(() => setValue({name: '', email: '', message: ''}))
-        .catch(error => alert(error));
-    }
+  const onSubmit = (data) => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      // body: encode({
+      //   "form-name": form.getAttribute("name"),
+      //   ...values
+      // })
+      body: data
+    })
+      .then(() => navigate("/thanks/"))
+      .then(() => reset())
+      .catch(error => alert(error));
+  }
   return (
     <form
       className="contact-form"
       name="contact"
       method="POST"
-      action="/thanks/"
       data-netlify="true"
       data-netlify-honeypot="bot-field"
-      onSubmit={handleSubmit}
+      onSubmit={handleSubmit(onSubmit)}
     >
       <input type="hidden" name="form-name" value="contact" />
-        <span className="required">必須</span><label htmlFor="name">お名前</label>
-        <br />
-        <input type="text" id="name" className="form-input" required name="name" placeholder="お名前" value={values.name} onChange={handleChange} />
-        <span className="required">必須</span><label htmlFor="email">メールアドレス</label>
-        <br />
-        <input type="email" id="email" className="form-input" name="email" placeholder="Email" value={values.email} onChange={handleChange} />
-        <span className="required">必須</span><label htmlFor="message">お問い合わせ内容</label>
-        <br />
-        <textarea id="message" name="message" className="form-input form-textarea" required placeholder="お問合せ内容" value={values.message} onChange={handleChange} />
-        <button className="submit-btn" type="submit">送信</button>
+      <span className="required">必須</span>
+      <label htmlFor="name">お名前</label>
+      <br />
+      <input
+        type="text"
+        id="name"
+        name="name"
+        className="form-input"
+        placeholder="お名前"
+        ref={register({ required: true })} />
+      {errors.name && <div className="form-error">お名前を入力してください</div>}
+      <span className="required">必須</span>
+      <label htmlFor="email">メールアドレス</label>
+      <br />
+      <input
+        type="email"
+        id="email"
+        name="email"
+        className="form-input"
+        placeholder="Email"
+        ref={register({ required: true })} />
+      {errors.email && <div className="form-error">メールアドレスを入力してください</div>}
+      <span className="required">必須</span>
+      <label htmlFor="message">お問い合わせ内容</label>
+      <br />
+      <textarea
+        id="message"
+        name="message"
+        className="form-input form-textarea"
+        placeholder="お問合せ内容"
+        ref={register({ required: true })} />
+      {errors.message && <div className="form-error">お問い合わせ内容を入力してください</div>}
+      <button className="submit-btn" type="submit">送信</button>
     </form>
   )
 }
