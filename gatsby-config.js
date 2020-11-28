78630e8ab101b63e5664dc5fe0a00429726ab3d1
Administrator <admin@example.com>
2020-11-28 13:33:27 +0900

    image追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,15 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    'gatsby-plugin-react-helmet'
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
   ],
 }
--- src/pages/components/FirstView.js
+++ src/pages/components/FirstView.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Image from '../components/Image';
 
 const FirstView = () => {
   return (
     <section className="first-view">
       <div>first view page</div>
+      <div>
+        <Image />
+      </div>
     </section>
   )
 }
